a856042f5cafa8bee73803a5a85bc84197ab1b29
sumania95 <ayansumania95@gmail.com>
2022-04-11 08:53:58 +0800

    final

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,23 +1,3 @@
-"""
-WSGI config for main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-# activate_this = 'D:/Project/file_management/env/Scripts/activate_this.py'
-# # execfile(activate_this, dict(__file__=activate_this))
-# exec(open(activate_this).read(),dict(__file__=activate_this))
-#
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-#
-# application = get_wsgi_application()
-
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
